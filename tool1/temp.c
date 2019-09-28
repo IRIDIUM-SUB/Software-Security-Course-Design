c9e2a23a07fe203c680a7dda7a96a25d81025708
POTASSIUM7429 <32727642+POTASSIUM7429@users.noreply.github.com>
2019-09-28 20:09:13 +0800

    Tool1-Config For Flex

---
+++ tool1/temp.c
@@ -0,0 +1,21 @@
+int main(int argc,char** argv){
+int res= foo(argc);
+if (res==42)
+{
+    return 1;
+}
+else
+{
+    return 0;
+}
+}
+int foo(int para)
+{
+    if (para==1)
+    return 42;
+    else
+    {
+        return 43;
+    }
+    
+}
