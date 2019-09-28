8ef43aea2f951a66b6f42dae363d0063f965e8d8
POTASSIUM7429 <32727642+POTASSIUM7429@users.noreply.github.com>
2019-09-28 20:19:02 +0800

    Tool3 for Vulnerability detection

---
+++ temp/2019-09-28_16_52_13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void kks(int a,int b,long c)
+{
+    memncpy(a,b,c);
+}
+int main(){
+int kksk;
+int pdse;
+long rous;
+
+strncpy(kksk,pdse,rous);
+strcat(pdse,rous);
+
+memcpy(kksk,pdse,rous);
+strcpy(pdse,kksk);
+strncat(kksk,pdse,rous);
+return 0;
+}
