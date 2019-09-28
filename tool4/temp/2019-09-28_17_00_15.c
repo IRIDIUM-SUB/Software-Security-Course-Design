1fe89455256e4607cab51bbdb8213b590c759292
POTASSIUM7429 <32727642+POTASSIUM7429@users.noreply.github.com>
2019-09-28 20:23:34 +0800

    Tool4 for Multi-thread

---
+++ tool4/temp/2019-09-28_17_00_15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void kks(int a,int b,long c)
+{
+    memncpy(a,b,c);
+}
+int main(){
+int kksk;
+int pdse;
+long rous;
+strncat(kksk,pdse,rous);
+strncpy(kksk,pdse,rous);
+memcpy(kksk,pdse,rous);
+strcpy(pdse,kksk);
+strcat(pdse,rous);
+
+return 0;
+}
